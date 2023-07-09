f57b950886cc46aaf1b05d5198d4a5ce9ebc016a
nowarlove <2001020019@student.umrah.ac.id>
2023-07-09 15:07:42 +0700

    update fitur

--- lib/traceroute.dart
+++ lib/traceroute.dart
@@ -1,12 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_traceroute/flutter_traceroute.dart';
+import 'package:flutter_traceroute/flutter_traceroute_platform_interface.dart';
 
-class TraceroutePage extends StatelessWidget {
-  const TraceroutePage({super.key});
+class TraceroutePage extends StatefulWidget {
+  const TraceroutePage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _TraceroutePageState createState() => _TraceroutePageState();
+}
+
+class _TraceroutePageState extends State<TraceroutePage> {
+  String domain = '';
+  List<String> tracerouteResults = [];
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Tracerout page deh"),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            decoration: const InputDecoration(
+              labelText: 'Domain Name',
+              hintText: 'Enter a domain name',
+            ),
+            onChanged: (value) {
+              setState(() {
+                domain = value;
+              });
+            },
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _performTraceroute();
+          },
+          child: const Text('Perform Traceroute'),
+        ),
+        const SizedBox(height: 16),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                for (String result in tracerouteResults) Text(result),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
+  }
+
+  Future<void> _performTraceroute() async {
+    if (domain.isNotEmpty) {
+      tracerouteResults.clear();
+      final traceroute = FlutterTraceroute();
+
+      try {
+        final List<dynamic> hops =
+            await traceroute.trace(domain as TracerouteArgs).toList();
+
+        // ignore: unused_local_variable
+        for (TracerouteStep hop in hops) {
+          setState(() {
+            // ignore: prefer_typing_uninitialized_variables
+            var hop;
+            tracerouteResults.add('Hop: ${hop.hop}, IP: ${hop.ip}');
+          });
+        }
+      } catch (e) {
+        setState(() {
+          tracerouteResults.add('Error: $e');
+        });
+      }
+    }
   }
 }
--- lib/whois.dart
+++ lib/whois.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:whois/whois.dart';
 
-class WhoisPage extends StatelessWidget {
-  const WhoisPage({super.key});
+class WhoisPage extends StatefulWidget {
+  const WhoisPage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _WhoisPageState createState() => _WhoisPageState();
+}
+
+class _WhoisPageState extends State<WhoisPage> {
+  String domain = '';
+  String result = '';
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Whoooooooooooisssssss"),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            decoration: const InputDecoration(
+              labelText: 'Domain Name',
+              hintText: 'Enter a domain name',
+            ),
+            onChanged: (value) {
+              setState(() {
+                domain = value;
+              });
+            },
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _getWhoisInfo();
+          },
+          child: const Text('Check Whois'),
+        ),
+        const SizedBox(height: 16),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Text(result),
+          ),
+        ),
+      ],
     );
+  }
+
+  Future<void> _getWhoisInfo() async {
+    if (domain.isNotEmpty) {
+      final data = await Whois.lookup(domain);
+      setState(() {
+        result = data.toString();
+      });
+    }
   }
 }
